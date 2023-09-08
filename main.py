955fb18cf253664febeb674d545b4c1b68a16a0f
1A2414A3D1195BDBDBA4F2E66A3B490B <144415218+1A2414A3D1195BDBDBA4F2E66A3B490B@users.noreply.github.com>
2023-09-08 13:53:23 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-def factorial(n):
-  if n == 0:
+# 1.1 implement a recursive function to calculate the factorial of a given number.
+
+def fact_rec(n):
+  if n==0 or n==1:
     return 1
   else:
-    return n * factorial(n - 1)
+    return n*fact_rec(n-1)
+   
+number =2
+res = fact_rec(number)
 
-
-print(factorial(10))
+print("the factorial of {} is {}". format(number, res))
